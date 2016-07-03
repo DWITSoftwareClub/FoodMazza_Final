07427083f4dc04f2b924f680de84abce7b895f3a
Harris Kunwar <Harris Kunwar>
2016-07-03 16:25:31 +0530

    First Commit

---
+++ test.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>contactus</title>
+<link rel="stylesheet" type="text/css" href="file:///E|/Xampp/htdocs/test/style.css">
+<link rel="shortcut icon" href="file:///E|/Xampp/htdocs/test/images/contact1.png" />
+</head>
+<body>
+<div id="social">
+<font class="contact" size="+1"><b>Welcome to Online Department Store.</b></font>
+<a href="https://www.facebook.com/?_rdr" target="_blank"><img src="file:///E|/Xampp/htdocs/test/images\twit.gif" height="30px" width="30px" align="right"></a><a href="https://www.twitter.com" target="_blank"> <img src="file:///E|/Xampp/htdocs/test/images/fb.gif" height="30px" width="30px" align="right"></a></div>
+<div id="smenu">
+<ul>
+				<li><a href="file:///E|/Xampp/htdocs/test/index.html"><img class="navicon" src="file:///E|/Xampp/htdocs/test/images/home.png"> Home</a></li>
+				<li><a href="#"><img class="navicon" src="file:///E|/Xampp/htdocs/test/images/gallery.png"> Our Products</a></li>
+                <li><a href="#"><img class="navicon" src="file:///E|/Xampp/htdocs/test/images/map.png"> Our Location</a></li>
+				<li><a href="file:///E|/Xampp/htdocs/test/contactus.html" class="contact"><img class="navicon" src="file:///E|/Xampp/htdocs/test/images/contact.png">Contact us</a></li>
+</ul>
+</div></div>
+<div id="company">
+<div id="companylogo">
+<img src="file:///E|/Xampp/htdocs/test/images\logo.gif" height="110px" width="170px" align="left">
+</div>
+<div id="companyico">
+<img src="file:///E|/Xampp/htdocs/test/images\logo1.gif" height="110px" width="180px" align="right">
+</div>
+</div>
+<center>
+<div id="blank"></div>
+<div id="wrapper">
+<img src=""
+</div>
+<?php
+
+echo "Hello World";
+
+?>
